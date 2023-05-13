35e075a509a07f21d8db4d626c7666799670b82c
MohaammedGamal <mohaammed.gamal@gmail.com>
2023-05-13 17:41:28 +0300

    Introduction to JavaScript ES6 & Intemediate JavaScript

---
+++ 10. Intermediate JavaScript/main.js
@@ -0,0 +1,42 @@
+// // Generate random number
+// var randomNumber = Math.floor((Math.random() * 100) + 1);
+// console.log("The percentage that you will achieve what you want is " + randomNumber + " %");
+
+
+// // Control flow
+// var score = 79
+// if (score < 75) {
+//     console.log("Failed ...")
+// } else if (score > 75 && score <= 80) {
+//     console.log("Passive ...")
+// } else {
+//     console.log("Failed ...")
+// }
+// var value = 5;
+// switch (value) {
+//     case 1:
+//         console.log(1)
+//         break;
+//     case 2:
+//         console.log(2)
+//         break;
+//     case 3:
+//         console.log(3)
+//         break;
+
+//     default:
+//         console.log("Unknown")
+// }
+
+// // arrays
+// var names = ["mohamed", "hatem", "amira"];
+// console.log(names[0]);
+// console.log(names.length);
+// names.push("youssif");
+// console.log(names);
+// console.log(names.length);
+// console.log(names.includes("mohamed"));
+// names[names.length] = "mahmoud";
+// console.log(names);
+
+
