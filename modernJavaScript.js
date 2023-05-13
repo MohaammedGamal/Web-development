030deddb62544772a9fb9ed66b7a7ae65a7d01e3
MohaammedGamal <mohaammed.gamal@gmail.com>
2023-05-13 18:55:13 +0300

    adding modernJavaScript test file - out of the course content

---
+++ modernJavaScript.js
@@ -0,0 +1,115 @@
+// const showName = name => {
+//   console.log(name)
+// }
+
+// // showName("mohamed");
+
+// /*************************/
+
+// const result = (num1, num2) => num1 * num2
+
+// let test = result(5, 5);
+// // console.log(test)
+
+// /*************************/
+
+// // class maleHuman {
+// //   constructor(){
+// //     this.gender = "male"
+// //   }
+// // }
+
+// // class Person extends maleHuman {
+// //   constructor(){
+// //     super();
+// //     this.name = "mohamed";
+// //   }
+// //   printName(){
+// //     console.log(this.name)
+// //   }
+// //   printGender(){
+// //     console.log(this.gender)
+// //   }
+// // }
+
+// // let mohamed = new Person();
+// // mohamed.printName();
+// // mohamed.printGender();
+
+
+// class maleHuman {
+
+//     gender = "male";
+
+// }
+
+// class Person extends maleHuman {
+  
+//     name = "mohamed";
+
+//   }
+//   printName = () => {
+//     console.log(this.name)
+//   }
+//   printGender = () => {
+//     console.log(this.gender)
+//   }
+// }
+
+// let mohamed = new Person();
+// mohamed.printName();
+// mohamed.printGender();
+
+
+
+// const numbers = [1, 2, 3]
+// const newNumbers = [...numbers, 4, 5]
+// console.log(newNumbers)
+
+// const Person = {
+//   name: "mohamed"
+// }
+// const newPerson = {
+//   ...Person,
+//   age: 28
+// }
+
+// console.log(newPerson);
+
+
+// const filter = (...a) => {
+//   return a.filter(el => el === 1)
+// }
+
+// console.log(filter(1, 2, 3))
+
+// const [first, last] = new Array("mohamed", "gamal")
+// console.log(first);
+// console.log(last)
+
+// primitive types
+//   number
+//   int
+//   string
+// reference types
+//   arrays
+//   objects
+
+// const numbers = [1, 2, 3]
+// const doubledNumbers = numbers.map((num) => {
+//   return num * num;
+// })
+
+// console.log(doubledNumbers);
+
+
+
+
+
+
+
+
+
+
+
+
